96cf903d760014e223c7993c8438e87fe4f03cb0
Antreo Pukay <acrosswalker@yahoo.com>
2018-11-01 11:23:50 -0700

    completed app file

---
+++ src/components/app/App.js
@@ -0,0 +1,46 @@
+import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import { PropTypes } from 'prop-types';
+// import { connect } from 'react-redux';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+
+
+class App extends PureComponent {
+
+  static propTypes = {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+    return (
+      <Router>
+
+        <div className="app-container">
+          <Header/>
+          <main>
+            <div className="switch">
+              <Switch>
+                <Route exact path="/" component={Home}/>
+
+                <Redirect to="/"/>
+              </Switch>
+            </div>
+          </main>
+
+          <Footer/>
+        </div>
+
+      </Router>
+    );
+  }
+
+}
+
+export default App;
